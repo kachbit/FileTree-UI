865c89f0d86aa9f9c491e7ad9579c5ff09a12335
rocketblue <59241904+kachbit@users.noreply.github.com>
2022-06-16 12:16:37 -0700

    Update ftree.js

--- src/ftree.js
+++ src/ftree.js
@@ -8,7 +8,9 @@ class fTree {
         const tree = this.generateTree(json);
         // append the tree to the body
         container.appendChild(tree);
-        container.querySelector('section').style.marginLeft = "18px" 
+        container.querySelector('section').style.marginLeft = "18px";
+        container.querySelector('section').id = "root";
+        container.classList.add('treeContainer')
         // make each div element collapsable
         const divs = container.querySelectorAll("div");
         divs.forEach((div) => {
@@ -21,12 +23,12 @@ class fTree {
 
         nodes.forEach((node) => {
             const arrow1 = document.createElement("span");
-            arrow1.innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 200 200" style="transform: rotate(-90deg);margin-right:6px" xml:space="preserve"><polygon points="199.404,63.993 171.12,35.709 99.702,107.127 28.284,35.709 0,63.993 99.702,163.695 "/></svg>`;
+            arrow1.innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 200 200" style="transform: rotate(-90deg);margin-right:6px;fill:white;background: transparent;" xml:space="preserve"><polygon points="199.404,63.993 171.12,35.709 99.702,107.127 28.284,35.709 0,63.993 99.702,163.695 "/></svg>`;
             arrow1.id = "arrow1";
             node.prepend(arrow1);
 
             const arrow2 = document.createElement("span");
-            arrow2.innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 200 200" style="margin-right:6px" xml:space="preserve"><polygon points="199.404,63.993 171.12,35.709 99.702,107.127 28.284,35.709 0,63.993 99.702,163.695 "/></svg>`;
+            arrow2.innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 200 200" style="margin-right:6px;fill:white;background: transparent;" xml:space="preserve"><polygon points="199.404,63.993 171.12,35.709 99.702,107.127 28.284,35.709 0,63.993 99.702,163.695 "/></svg>`;
             arrow2.id = "arrow2";
 
             node.prepend(arrow2);
@@ -43,10 +45,12 @@ class fTree {
     }
     generateTree(json) {
         const section = document.createElement("section");
+        section.classList.add('treeSection')
         const keys = Object.keys(json);
         keys.forEach((key) => {
             const span = document.createElement("span");
-            span.innerHTML = "<div>" + key + "</div>";
+            span.classList.add('treeSpan');
+            span.innerHTML = "<div class='treeDiv'>" + key + "</div>";
             if (typeof json[key] === "object") {
                 span.querySelector("div").classList.add("folder");
                 span.appendChild(this.generateTree(json[key]));
